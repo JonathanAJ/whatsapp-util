0b330d6104213a384f36a235124bc33af1ac9621
Jonathan Alves <alves_aj@windowslive.com>
2019-01-29 16:15:11 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormMain from './components/form-main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="App page">
+        <div class="header ">
+          <header class="page-header ">
+            <div class="page-header__inner">
+              <a class="page-header__logo" href="https://www.whatsapp.com"></a>
+              Not Official Site
+            </div>
+          </header>
+        </div>
+        <div class="page-main">
+          <div class="page-main__inner">
+              <div class="page-content page-content--single">
+              <div class="block block--send">
+                <h1 id="action-title" class="block__title">Open WhatsApp Web or Mobile</h1>
+                <FormMain />        
+              </div>
+              </div>
+          </div>
+        </div>
+      </div >
     );
   }
 }
